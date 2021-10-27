c3aaba3c24f390099c2e9843ee6f3c292c6b41d5
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-10-27 13:02:42 +0200

    Switch ens external test to ens-contracts repo

--- test/externalTests/ens.sh
+++ test/externalTests/ens.sh
@@ -27,14 +27,14 @@ source test/externalTests/common.sh
 verify_input "$1"
 export SOLJSON="$1"
 
-function compile_fn { npx truffle compile; }
-function test_fn { npm run test; }
+function compile_fn { yarn build; }
+function test_fn { yarn test; }
 
 function ens_test
 {
-    local repo="https://github.com/ensdomains/ens.git"
+    local repo="https://github.com/ensdomains/ens-contracts.git"
     local branch=master
-    local config_file="truffle-config.js"
+    local config_file="hardhat.config.js"
     local min_optimizer_level=1
     local max_optimizer_level=3
 
@@ -45,20 +45,18 @@ function ens_test
     setup_solcjs "$DIR" "$SOLJSON"
     download_project "$repo" "$branch" "$DIR"
 
-    # Use latest Truffle. Older versions crash on the output from 0.8.0.
-    force_truffle_version ^5.1.55
-
     neutralize_package_lock
     neutralize_package_json_hooks
-    force_truffle_compiler_settings "$config_file" "${DIR}/solc" "$min_optimizer_level"
-    npm install
+    force_hardhat_compiler_binary "$config_file" "$SOLJSON"
+    force_hardhat_compiler_settings "$config_file" "$min_optimizer_level"
+    yarn install
 
     replace_version_pragmas
-    force_solc_modules "${DIR}/solc"
+    neutralize_packaged_contracts
 
     for level in $selected_optimizer_levels; do
-        truffle_run_test "$config_file" "${DIR}/solc" "$level" compile_fn test_fn
+        hardhat_run_test "$config_file" "$level" compile_fn test_fn
     done
 }
 
-external_test Ens ens_test
+external_test ENS ens_test
