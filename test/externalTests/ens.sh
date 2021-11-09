d029fa6a63b2cf9bf2f13b4893a13111816823f1
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-11-09 17:13:36 +0100

    fixup! Switch ens external test to ens-contracts repo

--- test/externalTests/ens.sh
+++ test/externalTests/ens.sh
@@ -33,7 +33,7 @@ function test_fn { yarn test; }
 function ens_test
 {
     local repo="https://github.com/ensdomains/ens-contracts.git"
-    local branch=master
+    local branch="v0.0.5"
     local config_file="hardhat.config.js"
     local min_optimizer_level=2
     local max_optimizer_level=3
